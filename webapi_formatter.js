1efa30cd2e008bad6b0b551423a3869434d6ee3e
Nina Caparros <56190177+ncaparros@users.noreply.github.com>
2021-11-06 16:40:36 +0100

    typo

--- webapi_formatter.js
+++ webapi_formatter.js
@@ -7,7 +7,7 @@ function formatJSON() {
 }
 
 button.addEventListener("click", async () => {
-  var [tab] = await chrome.tabs.query({ active : true, currectWindow : true});
+  var [tab] = await chrome.tabs.query({ active : true, currentWindow : true});
   chrome.scripting.executeScript({
     target : { tabId : tab.id},
     function : formatJSON,
