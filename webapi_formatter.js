eee6314119617cb3e0739bbf1705f8d5927094e9
ncaparros <mymdp4github>
2021-11-06 16:47:38 +0100

    style and format

--- webapi_formatter.js
+++ webapi_formatter.js
@@ -3,7 +3,7 @@ var button = document.getElementById("btn");
 function formatJSON() {
   var bodyContent = document.getElementsByTagName("body").item(0).innerText;
   var formattedContainer = document.getElementsByTagName("pre").item(0);
-  formattedContainer.innerText = JSON.stringify(JSON.parse(body2)).replace('[', '\n[\n\t').replace(']', '\n]').replaceAll('},','\n\t},\n\n\t').replaceAll('\",\"','\",\n\t\t\"');
+  formattedContainer.innerText = JSON.stringify(JSON.parse(bodyContent)).replace('[', '\n[\n\t').replace(']', '\n]').replaceAll('},','\n\t},\n\n\t').replaceAll(',\"',',\n\t\t\"');
 }
 
 button.addEventListener("click", async () => {
