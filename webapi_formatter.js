a7a9c4337d106fbdc94e8b418a643a50434dee02
Nina Caparros <56190177+ncaparros@users.noreply.github.com>
2021-11-06 16:24:17 +0100

    Update webapi_formatter.js

--- webapi_formatter.js
+++ webapi_formatter.js
@@ -1,3 +1,15 @@
-var bodyContent = document.getElementsByTagName("body").item(0).innerText;
-var formattedContainer = document.getElementsByTagName("pre").item(0);
-formattedContainer.innerText = JSON.stringify(JSON.parse(body2)).replace('[', '\n[\n\t').replace(']', '\n]').replaceAll('},','\n\t},\n\n\t').replaceAll('\",\"','\",\n\t\t\"');
+var button = document.getElementById("btn");
+
+function formatJSON() {
+  var bodyContent = document.getElementsByTagName("body").item(0).innerText;
+  var formattedContainer = document.getElementsByTagName("pre").item(0);
+  formattedContainer.innerText = JSON.stringify(JSON.parse(body2)).replace('[', '\n[\n\t').replace(']', '\n]').replaceAll('},','\n\t},\n\n\t').replaceAll('\",\"','\",\n\t\t\"');
+}
+
+button.addEventListener("click", async () => {
+  var [tab] = await chrome.tabs.query({ active : true, currectWindow : true});
+  chrome.scripting.executeScript({
+    target : { tabId : tab.id},
+    function : formatJSON,
+  });
+});
